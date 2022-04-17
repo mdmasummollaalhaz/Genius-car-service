7ec58e011f7a2f0c0e85708f3967ea4846edeade
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-17 09:49:25 +0600

    set env variable for firebase config

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
-import Services from './Pages/Home/Services/Services';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import NotFound from './Pages/Shared/NotFound/NotFound';
 import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
         <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
 
+
 const Register = () => {
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    const navigateLogin = () =>{
+        navigate('/login')
+    }
+
+    const handleRegister = event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        console.log(name, email, password);
+    }
     return (
-        <div>
-            <h2>Please, Register</h2>
+        <div className='container w-50 mx-auto mt-5 register-form'>
+            <h2 style={{textAlign: 'center'}}>Please, Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" ref={nameRef} name="name" id="" placeholder='Your name' required/>
+                <input type="email" ref={emailRef} name="email" id="" placeholder='Your email' required/>
+                <input type="password" ref={passwordRef} name="password" id="" placeholder='password' required/>
+                <input type="submit" value="Register" />
+            </form>
+      <p>Already have an account? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please, login</Link></p>
         </div>
     );
 };
