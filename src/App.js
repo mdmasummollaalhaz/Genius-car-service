385b2793000a824a9d01446c4380e980f12bb0dc
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-17 08:13:02 +0600

    dynamic route and access route params

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
 import Services from './Pages/Home/Services/Services';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 
 function App() {
   return (
@@ -12,8 +13,8 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path='/about' element={<About></About>}></Route>
-        {/* <Route path='/services' element={<Services></Services>}></Route> */}
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Service.css';
 
 const Service = ({service}) => {
-    const {name, img, description, price} = service;
+    const {id, name, img, description, price} = service;
+    const navigate = useNavigate();
+    const navigateToServiceDetail = id =>{
+        navigate(`/service/${id}`);
+    }
     return (
         <div className='service'>
             <img className='img-fluid' src={img} alt="" />
             <h2>{name}</h2>
             <p>Price:{price}</p>
             <p>{description}</p>
-            <button className='book'>Book: {name}</button>
+            <button onClick={() => navigateToServiceDetail(id)} className='book'>Book: {name}</button>
         </div>
     );
 };
