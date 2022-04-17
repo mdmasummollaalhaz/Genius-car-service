8e99b0754c61468d997c09a00c9959fa51ff019e
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-17 06:34:21 +0600

    banner

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -12,7 +12,7 @@ const Services = () => {
   }, []);
   return (
     <div className='container'>
-      <h2 className="services-title">Our Services: {services.length}</h2>
+      <h2 className="services-title mt-5 mb-3">Our Services: {services.length}</h2>
       <div className="services-container">
         {services.map(service => (
           <Service key={service.id} service={service}></Service>
